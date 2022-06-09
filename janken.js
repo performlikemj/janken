ce02375d6286ec753d7520c36990d48b19dc1f86
MJ <Ferris@MJs-Macbook.local>
2022-06-09 21:12:03 +0900

    Fix array length format for computerPlay

--- janken.js
+++ janken.js
@@ -2,7 +2,7 @@
 /*  have an array of options for computer: rock, paper, and scissors */
 function computerPlay() {
     const options = ["rock", "paper", "scissors"];
-    const selection = options[Math.floor(Math.random() * length(options) + 1)];
+    const selection = options[Math.floor(Math.random() * options.length + 1)];
     return selection;
 }
 /*  computer picks one at random  and returns choice */
