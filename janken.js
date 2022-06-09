008631d23304ff08e28811e5b71a5700b5f75da2
MJ <Ferris@MJs-Macbook.local>
2022-06-09 21:24:07 +0900

    Align comments to code

--- janken.js
+++ janken.js
@@ -1,8 +1,8 @@
 /*  computerPlay */
-/*  have an array of options for computer: rock, paper, and scissors */
 function computerPlay() {
+    /*  have an array of options for computer: rock, paper, and scissors */
     const options = ["rock", "paper", "scissors"];
+    /*  computer picks one at random  and returns choice */
     const selection = options[Math.floor(Math.random() * options.length)];
     return selection;
 }
-/*  computer picks one at random  and returns choice */
