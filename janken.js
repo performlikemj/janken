9da3e94553854f247df4a7a55237364ee2be543f
MJ <Ferris@MJs-Macbook.local>
2022-06-09 22:54:51 +0900

    Create singleRound function with hardcoded choices

--- janken.js
+++ janken.js
@@ -22,4 +22,47 @@ function playerSelection() {
         playerSelection();
     }
 }
+
+const userTurn = playerSelection();
+const computerTurn = computerPlay();
+/*  single game */
+/*  takes two parameters, the playerSelection and computerSelection */
+function singleRound(userTurn, computerTurn) {
+    /*  output outcome of single game */
+    switch (userTurn === "rock") {
+        case (computerTurn === "rock"):
+            console.log("Tie");
+            return "Tie!";
+        case (computerTurn === "paper"):
+            console.log("You lose!");
+            return "You lose! Paper beats rock.";
+        case (computerTurn === "scissors"):
+            console.log("You win!");
+            return "You win! Rock beats scissors";
+    }
+
+    switch (userTurn === "paper") {
+        case (computerTurn === "paper"):
+            console.log("Tie");
+           return "Tie!";
+        case (computerTurn === "scissors"):
+            console.log("You lose!");
+            return "You lose! Scissors beats rock.";
+        case (computerTurn === "rock"):
+            console.log("You win!");
+            return "You win! paper beats rock";
+    }
+
+    switch (userTurn === "scissors") {
+        case (computerTurn === "scissors"):
+            console.log("Tie");
+           return "Tie!";
+        case (computerTurn === "rock"):
+            console.log("You lose!");
+            return "You lose! rock beats rock.";
+        case (computerTurn === "paper"):
+            console.log("You win!");
+            return "You win! Scissors beats rock";
+    }
     
+}
