b88e4b047fb89bb7e20b22a794651ab57afce9ad
MJ <Ferris@MJs-Macbook.local>
2022-06-09 21:22:56 +0900

    Fix selection variable by remove the + 1 at end because it is not needed in an array

--- janken.js
+++ janken.js
@@ -2,7 +2,7 @@
 /*  have an array of options for computer: rock, paper, and scissors */
 function computerPlay() {
     const options = ["rock", "paper", "scissors"];
-    const selection = options[Math.floor(Math.random() * options.length + 1)];
+    const selection = options[Math.floor(Math.random() * options.length)];
     return selection;
 }
 /*  computer picks one at random  and returns choice */
