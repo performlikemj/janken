f567fce5f31dfb39eb41d020799300a1dc6aa6b7
MJ <Ferris@MJs-Macbook.local>
2022-06-09 22:01:48 +0900

    Create playerSelection function for user to choose an option

--- janken.js
+++ janken.js
@@ -6,3 +6,20 @@ function computerPlay() {
     const selection = options[Math.floor(Math.random() * options.length)];
     return selection;
 }
+
+/*  playerSelection */
+/*  create a function that asks the player what they want to play */
+function playerSelection() {
+    let choice = prompt("Pick rock, paper, or scissors. Or quit to give up");
+    choice = choice.toLowerCase();
+/*  if selection is not equal to rock, paper, or scissors, or quit, ask user
+    to choose again */
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        return choice;
+    } else if (choice === "quit") {
+        return;
+    } else {
+        playerSelection();
+    }
+}
+    
